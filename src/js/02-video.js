2a8e1508cec688b95dcc984b3d83eb1f93502469
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-03-31 19:06:20 +0300

    v2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+console.log(Player);
+// const player = new Player('handstick', {
+//   id: '#vimeo-player',
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
