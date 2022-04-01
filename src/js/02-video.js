785352b0b0f3f998c43203572deeea34e9297950
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-04-01 15:18:36 +0300

    finish 2d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,34 @@
 import Player from '@vimeo/player';
+import _ from 'lodash.throttle';
 
-console.log(Player);
-// const player = new Player('handstick', {
-//   id: '#vimeo-player',
-//   width: 640,
-// });
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+let currentPlayingTime = {
+  cpt: 0,
+};
+
+const onPlay = function (data) {
+  currentPlayingTime.cpt = data.seconds;
+  console.log(currentPlayingTime);
+  localStorage.setItem('videoplayer-current-time', currentPlayingTime.cpt);
+};
+
+player.on('timeupdate', _(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
